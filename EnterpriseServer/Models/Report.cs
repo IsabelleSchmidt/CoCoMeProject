8c54a77d7c6c2021c1f5a7e5683fc952d5c1dbec
Isabelle Schmidt <isabelle.schmidt@student.hs-rm.de>
2022-02-18 16:34:23 +0100

    Added Report Model

---
+++ EnterpriseServer/Models/Report.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EnterpriseServer.Models
+{
+    public class Report
+    {
+        public int EnterpriseId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string Productname { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public DateTime DeliverDate { get; set; }
+
+        public string status { get; set; }
+
+    }
+}
