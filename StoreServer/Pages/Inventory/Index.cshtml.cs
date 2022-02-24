988675e38e38345efb16838e0afbd8b6ee4708c3
Isabelle Schmidt <isabelle.schmidt@student.hs-rm.de>
2022-02-24 17:01:09 +0100

    Save Inventory report as PDF

--- StoreServer/Pages/Inventory/Index.cshtml.cs
+++ StoreServer/Pages/Inventory/Index.cshtml.cs
@@ -26,5 +26,17 @@ namespace StoreServer.Pages.Inventory
         {
             InventoryItem = await _context.InventoryItem.Include(item => item.ItemIdentifier).ToListAsync();
         }
+
+        public IActionResult OnGetCreateReport()
+        {
+            /* Render URL to PDF anchor-convert-website-to-pdf
+             * speichert pdf in ordnerstruktur
+             */
+            var render = new IronPdf.ChromePdfRenderer();
+            var doc = render.RenderUrlAsPdf("https://localhost:7071/Inventory");
+            doc.SaveAs("InventoryReport.pdf");
+            return RedirectToAction(actionName: "https://localhost:7071/Inventory");
+
+        }
     }
 }
